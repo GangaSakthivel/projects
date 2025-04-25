62b251adca6555f0a7ff13b0f97438d6481f81f6
GangaSakthivel <gangasakthivel22@gmail.com>
2025-04-25 17:08:22 +0530

    Student management application

---
+++ studentUnivercity/src/main/java/com/example/studentUnivercity/StudentUnivercityApplication.java
@@ -0,0 +1,13 @@
+package com.example.studentUnivercity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentUnivercityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentUnivercityApplication.class, args);
+	}
+
+}
---
+++ studentUnivercity/src/test/java/com/example/studentUnivercity/StudentUnivercityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.studentUnivercity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentUnivercityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
