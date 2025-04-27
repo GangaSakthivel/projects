e104d6303de37e78c4e858728019fec8bbe43137
GangaSakthivel <gangasakthivel22@gmail.com>
2025-04-27 21:14:55 +0530

    Student management

--- studentUnivercity/src/main/java/com/example/studentUnivercity/controller/CourseController.java
+++ studentUnivercity/src/main/java/com/example/studentUnivercity/controller/CourseController.java
@@ -6,10 +6,14 @@ import com.example.studentUnivercity.DTO.CourseResponseDTO;
 import com.example.studentUnivercity.service.CourseService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -20,8 +24,15 @@ public class CourseController {
     @Autowired
     private CourseService courseService;
 
+    @PostMapping("/input")
     public ResponseEntity<CourseResponseDTO> createCourse(@Valid @RequestBody CourseRequestDTO courseRequestDTO){
         CourseResponseDTO createResponse = courseService.createCourse(courseRequestDTO);
         return ResponseEntity.ok(createResponse);
+    }
+
+    public ResponseEntity<List<CourseResponseDTO>> getAllCourses(){
+        CourseResponseDTO responseDTO = courseService.getAllCourses();
+        return new ResponseEntity<>(responseDTO, HttpStatus.OK);
+
     }
 }
--- studentUnivercity/src/main/java/com/example/studentUnivercity/service/CourseService.java
+++ studentUnivercity/src/main/java/com/example/studentUnivercity/service/CourseService.java
@@ -2,10 +2,14 @@ package com.example.studentUnivercity.service;
 
 import com.example.studentUnivercity.DTO.CourseRequestDTO;
 import com.example.studentUnivercity.DTO.CourseResponseDTO;
+import com.example.studentUnivercity.model.Course;
+import com.example.studentUnivercity.model.University;
 import com.example.studentUnivercity.repository.CourseRepository;
 import com.example.studentUnivercity.repository.UniversityRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class CourseService {
@@ -18,11 +22,39 @@ public class CourseService {
 
     public CourseResponseDTO createCourse(CourseRequestDTO courseRequestDTO) {
 
-        CourseResponseDTO courseResponseDTO = universityRepository.findById(id);
+        University university = universityRepository.findById(courseRequestDTO.getUniversityId())
+                .orElseThrow(() -> new RuntimeException("University not found with the ID: " + courseRequestDTO.getUniversityId()));
 
+        //request to entity conversion
+        Course course = new Course();
 
+        course.setUniversity(university);
+        course.setName(courseRequestDTO.getName());
+        course.setDescription(courseRequestDTO.getDescription());
+        course.setCredits(courseRequestDTO.getCredits());
+        course.setSemester(courseRequestDTO.getSemester());
+        course.setStartDate(courseRequestDTO.getStartDate());
+        course.setEndDate(courseRequestDTO.getEndDate());
+        course.setDuration(courseRequestDTO.getDuration());
+        course.setLevel(courseRequestDTO.getLevel());
 
+        //course entity to response
 
+        CourseResponseDTO responseDTO = new CourseResponseDTO();
+        responseDTO.setName(course.getName());
+        responseDTO.setDescription(course.getDescription());
+        responseDTO.setCredits(course.getCredits());
+        responseDTO.setSemester(course.getSemester());
+        responseDTO.setStartDate(course.getStartDate());
+        responseDTO.setEndDate(course.getEndDate());
+        responseDTO.setDuration(course.getDuration());
+        responseDTO.setLevel(course.getLevel());
+
+        //settting up the fields that we want in response
+        responseDTO.setUniversityName(course.getUniversity().getUniversityName());
+        responseDTO.setUniversityWebsite(course.getUniversity().getWebsite());
+
+        return responseDTO;
 
     }
 }
