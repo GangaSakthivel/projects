c924d24cd69b66b5a56891afd8a77cb902f60d28
GangaSakthivel <gangasakthivel22@gmail.com>
2025-04-28 12:00:32 +0530

    Vehicle management

---
+++ VehicleDocumentManagement/src/main/java/com/example/VehicleDocumentManagement/VehicleDocumentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.VehicleDocumentManagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VehicleDocumentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VehicleDocumentManagementApplication.class, args);
+	}
+
+}
---
+++ VehicleDocumentManagement/src/test/java/com/example/VehicleDocumentManagement/VehicleDocumentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.VehicleDocumentManagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VehicleDocumentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
