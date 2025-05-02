8e222694918514efc458a4dce0c8ff05d8ef4c58
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-02 08:38:33 +0530

    Cage application

---
+++ cageApplication/src/test/java/com/example/cageApplication/CageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cageApplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CageApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
