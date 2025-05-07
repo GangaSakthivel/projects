346c614c8ed49614c801667affedf3768d760cb4
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-07 14:56:06 +0530

    spring project

---
+++ UserAuthenticationSpring/src/test/java/com/example/UserAuthenticationSpring/UserAuthenticationSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.UserAuthenticationSpring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthenticationSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
