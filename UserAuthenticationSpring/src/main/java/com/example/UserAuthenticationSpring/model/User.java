c9da606566e5346fb5927ad175020f84d8e352e6
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-08 16:45:40 +0530

    spring project

--- UserAuthenticationSpring/src/main/java/com/example/UserAuthenticationSpring/model/User.java
+++ UserAuthenticationSpring/src/main/java/com/example/UserAuthenticationSpring/model/User.java
@@ -1,30 +1,36 @@
 package com.example.UserAuthenticationSpring.model;
 
+
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 @Getter
 @Setter
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 @Table(name = "users")
 public class User {
 
-        @Id
-        @GeneratedValue(strategy = GenerationType.IDENTITY)
-        private Long id;
-        private String name;
-        private Long phoneNumber;
-        private Double salary;
-        private String password;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String phoneNumber;
+    private Double salary;
+    private Status status;
+    private String password;
 
-        @Enumerated(EnumType.STRING)
-        private Status status;
+    @ManyToMany(fetch = FetchType.EAGER, cascade = CascadeType.ALL)
+    private Set<Role> roles = new HashSet<>(); //many users can have multiple roles
 
 
+    //Employee ID (Auto generated),
+    //Employee Name, Phone Number, Salary and Account Status.
 }
-
