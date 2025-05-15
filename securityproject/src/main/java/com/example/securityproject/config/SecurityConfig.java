d1c1cb74c91315c0b2d50063f8c3b4823f00a6a6
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-15 16:08:22 +0530

    spring security

--- securityproject/src/main/java/com/example/securityproject/config/SecurityConfig.java
+++ securityproject/src/main/java/com/example/securityproject/config/SecurityConfig.java
@@ -1,2 +1,50 @@
-package com.example.securityproject.config;public class SecurityConfig {
+package com.example.securityproject.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(request-> request.anyRequest().authenticated())
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(session->
+                        session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .build();
+
+    }
+
+
+    public UserDetailsService userDetailsService(){
+        return new UserDetailsService() {
+            @Override
+            public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+
+
+                UserDetails user1 = User
+                        
+                return null;
+            }
+        }
+    }
+
+
+
+
+
+
 }
