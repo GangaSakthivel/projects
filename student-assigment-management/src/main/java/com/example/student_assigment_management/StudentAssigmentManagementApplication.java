3277d9e168e278744cd95c690423c439016f7d2c
GangaSakthivel <gangasakthivel22@gmail.com>
2025-05-19 15:10:56 +0530

    Spring boot student assignment management application

---
+++ student-assigment-management/src/main/java/com/example/student_assigment_management/StudentAssigmentManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.student_assigment_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentAssigmentManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudentAssigmentManagementApplication.class, args);
+	}
+
+}
---
+++ student-assigment-management/src/test/java/com/example/student_assigment_management/StudentAssigmentManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.student_assigment_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentAssigmentManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
